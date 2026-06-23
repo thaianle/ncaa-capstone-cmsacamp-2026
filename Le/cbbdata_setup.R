0f5885030b6fa4ebb9413496b447342455c1ec72
Thai An Le <thaianle1102@gmail.com>
2026-06-23 15:01:49 -0400

    Perform additional data checks and EDA for players dataset

--- Le/cbbdata_setup.R
+++ Le/cbbdata_setup.R
@@ -10,6 +10,7 @@
                              #confirm_password = '***')
 
 rm(list = ls())
+library(tidyverse)
 library(cbbdata)
 
 # The "year/season" represents the year when the season concludes, based on
@@ -24,14 +25,4 @@ df_2024 <- cbd_torvik_player_season(year = 2024)
 df_2025 <- cbd_torvik_player_season(year = 2025)
 
 df <- bind_rows(df_2022, df_2023, df_2024, df_2025)
-
-# Summarize some variables
-table(df$pos)
-table(df$exp)
-colSums(is.na(df))
-# https://stackoverflow.com/questions/26273663/r-how-to-total-the-number-of-na-in-each-col-of-data-frame
-# There are some missing values. We will investigate them later.
-
-df |>
-  ggplot(aes(x = mpg)) +
-  geom_histogram()
+write.csv(df, "Le/player_stats.csv", row.names = FALSE)
