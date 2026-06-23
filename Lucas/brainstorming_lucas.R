edc0d7345ce0562242fd32015a03fb5ac03bf579
Kendall Lucas <kendalllucas724@icloud.com>
2026-06-23 14:26:46 -0400

    trying to add column which tells us if this is a new team for them (aka they transferred into the program this year)

--- Lucas/brainstorming_lucas.R
+++ Lucas/brainstorming_lucas.R
@@ -40,10 +40,59 @@ devtools::install_github("andreweatherman/cbbdata")
 # persistent log-in
 cbbdata::cbd_login()
 
+#start here
+library(cbbdata)
+library(dplyr)
+library(ggplot2)
+
+
+#code thanks to An!
 df_2022 <- cbd_torvik_player_season(year = 2022)
 
+df_2023 <- cbd_torvik_player_season(year = 2023)
 
+df_2024 <- cbd_torvik_player_season(year = 2024)
 
+df_2025 <- cbd_torvik_player_season(year = 2025)
+
+df <- bind_rows(df_2022, df_2023, df_2024, df_2025)
+
+#if team name is equal to the team name in the previous year
+transfer_df <- df |>
+  group_by(player) |>
+  filter(n_distinct(team) > 1) |>
+  ungroup()
+
+df <- df |>
+  mutate(has_transferred = player %in% transfer_df$player)
+
+#df <- df |>
+#  select(-is_transfer)
+
+#shows the difference in points scored by players who had transferred vs players who had not
+# didnt really do what i wanted so IGNORE
+#df |>
+#  ggplot(aes(x = is_transfer, y = g)) +
+#  geom_col() +
+#  facet_wrap(~year)
+
+# trying to make transfer out year/transfer in year
+# changed_team is the first year after transfer
+df <- df |>
+  arrange(id, year) |>
+  group_by(id) |>
+  mutate(prev_team = lag(team),
+         prev_year = lag(year),
+         #if they have a previous team AND 
+         #the year is the one right after the previous year AND 
+         #the team they're on is not the same as the previous team THEN
+         #changed_team is through IF NOT THEN
+         #changed_team is false 
+         changed_team = ifelse(!is.na(prev_team) &
+                                 year == (prev_year + 1) &
+                                 team != prev_team,
+                               TRUE,
+                               FALSE))
 
 
 
